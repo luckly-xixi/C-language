fdb752a436d58e18a89d63ce5f06265fe3fec5fe
luckly-xixi <105301098+luckly-xixi@users.noreply.github.com>
2022-10-28 20:08:28 +0800

    Add files via upload

---
+++ test_2022_10_23.c
@@ -0,0 +1,55 @@
+#define _CRT_SECURE_NO_WARNINGS 
+//实现一个对整形数组的冒泡排序
+
+#include<stdio.h>
+
+int main()
+
+{
+
+	int arr[5] = { 2,4,5,3,0 };
+
+	int i = 0;
+
+	int j = 0;
+
+	int tmp = 0;
+
+	for (i = 0; i < 4; i++)
+
+	{
+
+		for (j = 0; j < 4 - i; j++)
+
+		{
+
+			if (arr[j + 1] < arr[j])
+
+			{
+
+				tmp = arr[j];
+
+				arr[j] = arr[j + 1];
+
+				arr[j + 1] = tmp;
+
+			}
+
+		}
+
+	}
+
+	for (i = 0; i < 5; i++)
+
+	{
+
+		printf("%d\n", arr[i]);
+
+	}
+
+
+
+	return 0;
+
+}
+
